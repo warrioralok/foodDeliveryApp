d67a8137ea55073687c216f726d4425ad4ac64ec
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-10-13 22:24:51 +0530

    changes for today about functional component

--- App.js
+++ App.js
@@ -1,2 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+
+const Childcomponent = () =>{ 
+    return (
+        <h2 className="child">This is child component</h2>
+    )
+}
+
+const ParentComponent = () =>(
+    <>
+    <div className="parent">
+        <h1>This is Parent Element</h1>
+    </div>
+    <Childcomponent/>
+    </>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ParentComponent/>)
